3e90adc85424468fa2860beda602269aaabf861b
Britta <brittacarroll@gmail.com>
2016-04-19 16:45:45 -0400

    fixed hound violation

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -3,8 +3,6 @@ require 'rails_helper'
 RSpec.describe Artist, type: :model do
   it { should have_valid(:first_name).when("Lisa", "David") }
   it { should_not have_valid(:first_name).when(nil, "") }
-
   it { should have_valid(:last_name).when("Carroll", "Kim") }
   it { should_not have_valid(:last_name).when(nil, "") }
-
 end
