609b76148e17902c06d1d1abd6e6ae8657852aa0
Britta <brittacarroll@gmail.com>
2016-04-19 13:39:21 -0400

    Revert "devise for users"
    This reverts commit f894069224a85121c3643fba3e495335d0ce14cc.

--- Gemfile
+++ Gemfile
@@ -11,7 +11,6 @@ gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'devise'
-gem 'foundation-rails'
 
 
 group :development, :test do
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,11 +3,4 @@ class Artist < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  ARTISTS = [
-    "painter",
-    "scuptor",
-    "photographer",
-    "architect"
-  ]
 end
