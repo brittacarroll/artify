ed148145335fca292b70711cc9be7ba2887f1205
Britta <brittacarroll@gmail.com>
2016-04-19 12:47:24 -0400

    wip commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,21 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  devise_for :galleries
+  devise_for :artists
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # root "artworks#index"
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
   #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  # devise_scope :galleries do
+  #   get "galleries/sign_in" => "galleries/sessions#new"
+  #   get "galleries/sign_out" => "galleries/sessions#destroy"
+  # end
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  # devise_scope :artist do
+  #   get "artists/sign_in" => "artists/sessions#new"
+  #   get "artists/sign_up" => "artists/registrations#new"
+  #   get "artists/sign_out" => "artists/sessions#destroy"
+  # end
 end
---
+++ spec/support/database_cleaner.rb
@@ -0,0 +1,26 @@
+require 'database_cleaner'
+
+RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  config.backtrace_exclusion_patterns = [/.gem/, /.rubies/]
+  end
+end
