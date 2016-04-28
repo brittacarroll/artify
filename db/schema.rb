153c40865e64dc3706ce72d8ff52459d4795fb83
Britta <brittacarroll@gmail.com>
2016-04-28 14:52:34 -0400

    styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160423181644) do
+ActiveRecord::Schema.define(version: 20160428180603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20160423181644) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.text     "description"
   end
 
   add_index "galleries", ["email"], name: "index_galleries_on_email", unique: true, using: :btree
