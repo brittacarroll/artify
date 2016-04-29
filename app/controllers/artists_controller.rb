52d1f5657794a812a88f81fb310da5faa56ccde7
Britta <brittacarroll@gmail.com>
2016-04-29 12:39:54 -0400

    artists profile change

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -5,10 +5,6 @@ class ArtistsController < ApplicationController
     if !current_artist.nil?
       @artist = current_artist
       @profile = current_artist.artists_profile
-      @art_piece = Artwork.find_by(params[:id])
-      if @art_piece != nil
-        @artist_profile_1 = @art_piece.artist.artists_profile
-      end
     end
 
     @art = Artwork.all
